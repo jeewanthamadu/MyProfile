7d876843a574714a52f2cb2c80eb41a6b98b31af
jeewanthamadu <jeewanthamadu2000@gmail.com>
2022-03-04 01:38:25 +0530

    completed transformation

--- myprojects/SPA/assest/js/script.js
+++ myprojects/SPA/assest/js/script.js
@@ -1,2 +1,23 @@
+
+/*hide form*/
 document.getElementById("itemId").style.setProperty("Display","none","important");
 document.getElementById("customerId").style.setProperty("Display","none","important");
+
+/*transition*/
+document.getElementById("navOrderPage").addEventListener("click",function (){
+    document.getElementById("orderId").style.setProperty("Display","block","important");
+    document.getElementById("customerId").style.setProperty("Display","none","important");
+    document.getElementById("itemId").style.setProperty("Display","none","important");
+});
+
+document.getElementById("navCustomerPage").addEventListener("click",function (){
+    document.getElementById("customerId").style.setProperty("Display","block","important");
+    document.getElementById("orderId").style.setProperty("Display","none","important");
+    document.getElementById("itemId").style.setProperty("Display","none","important");
+});
+
+document.getElementById("navItemPage").addEventListener("click",function (){
+    document.getElementById("itemId").style.setProperty("Display","block","important");
+    document.getElementById("customerId").style.setProperty("Display","none","important");
+    document.getElementById("orderId").style.setProperty("Display","none","important");
+});
