73ab3148c21fe4c2a5ebc54b254fbc7afd934415
jeewanthamadu <jeewanthamadu2000@gmail.com>
2022-03-07 01:03:35 +0530

    add table data

--- myprojects/SPA/assest/jQuery/jQuery.js
+++ myprojects/SPA/assest/jQuery/jQuery.js
@@ -1,11 +1,12 @@
-/*_________customer part___________*/
+/*_________________customer part______________________*/
+
+/*_________customer save___________*/
 $("#btnCusAdd").click(function (){
 
     let customerId = $("#txtCusID").val();
     let customerName = $("#txtCusName").val();
     let customerAddress = $("#txtCusAddress").val();
     let customerSalary = $("#txtCusSalary").val();
-
 
     var customerOB ={
         id:customerId,
@@ -14,6 +15,14 @@ $("#btnCusAdd").click(function (){
         salary:customerSalary
     }
     customerDB.push(customerOB);
+    loadTableCusData();
+});
 
-
-})
+/*_________customer Table Load___________*/
+function loadTableCusData (){
+    $("#customerTableBody").empty();
+    for (var i of customerDB){
+        let raw = `<tr><td>${i.id}</td><td>${i.name}</td><td>${i.address}</td><td>${i.salary}</td></tr>`
+        $("#customerTableBody").append(raw);
+    }
+}
