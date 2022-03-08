0f36382b8e0dba8162d4d24869ae7d5b2717db34
jeewanthamadu <jeewanthamadu2000@gmail.com>
2022-03-08 23:40:03 +0530

    Add setter and getter

---
+++ myprojects/SPA/model/CustomerDTO.js
@@ -0,0 +1,38 @@
+function CustomerDTO (id,name,address,salary){
+var customerID=id;
+var customerName=name;
+var customerAddress=address;
+var customerSalary=salary;
+
+this.getCustomerID=function (){
+    return customerID;
+}
+this.setCustomerID=function (id){
+   customerID=id;
+}
+
+this.getCustomerName=function (){
+    return customerName ;
+}
+this.setCustomerName=function (name){
+    customerName=name;
+}
+
+this.getCustomerAddress=function (){
+    return customerAddress ;
+}
+this.setCustomerAddress=function (address){
+    customerAddress=address;
+}
+
+this.getCustomerSalary=function (){
+    return customerSalary ;
+}
+this.setCustomerSalary=function (salary){
+    customerSalary=salary;
+}
+
+
+
+
+}
