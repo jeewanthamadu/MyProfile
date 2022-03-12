a7ad5a5f93d63368156eb868a9ef370f3eef0103
jeewanthamadu <jeewanthamadu2000@gmail.com>
2022-03-12 22:44:23 +0530

    add to order table

--- myprojects/SPA/controller/orderController.js
+++ myprojects/SPA/controller/orderController.js
@@ -3,6 +3,9 @@
 generateOrderId();
 setDate();
 
+$("#btnOrderAdd").click(function (){
+    addCart();
+});
 
 /*_________load customer and item ids___________*/
 $("#orderCusIdCmb").change(function (e){
@@ -14,6 +17,7 @@ $("#itemIdCmb").change(function (e){
     let selectedItemId =$('#itemIdCmb').find(":selected").text();
     selectedItem(selectedItemId);
 });
+
 
 
 /*_________Auto Generate Order ID___________*/
@@ -89,8 +93,6 @@ function selectedCustomer(CustomerId){
             $("#txtAddress").val(element.getCustomerAddress());
         }
     }
-
-
 }
 
 /*__________set Date____________*/
@@ -99,5 +101,93 @@ function setDate() {
     let dd = d.toISOString().split("T")[0].split("-");
     $("#txtOrderDate").val(dd[0]+"-"+dd[1]+"-"+dd[2]);
     $("#txtOrderDate").text(dd[0]+"-"+dd[1]+"-"+dd[2]);
+}
 
+
+
+// ________________________add to cart_____________________
+
+var fullTotal =0;
+
+function addCart(){
+    let itemCode =selectedItemId;
+    let itemName=$("#txtOrderItemName").val();
+    let qtyOnHand=$("#txtQtyOnHand").val();
+    let price=$("#txtPrice").val();
+    let orderQty=$("#oQty").val();
+    let total= 0;
+
+    if (qtyOnHand >= orderQty) {
+        qtyOnHand = qtyOnHand - orderQty;
+    }else{
+        alert("Enter Valid QTY");
+        $("#oQty").val("");
+        return;
+    }
+
+    //___________________Updating qty_______________
+
+    for (let i = 0; i < itemDB.length; i++) {
+        if (id == itemDB[i].getItemID()) {
+            itemDB[i].setItemQty(qtyOnHand);
+        }
+    }
+
+    let newQty = 0;
+    let newTotal= 0;
+
+    if (checkDuplicates(id)==-1) {
+        total = orderQty * price;
+        fullTotal = fullTotal + total;
+        let row =
+            `<tr><td>${itemCode}</td><td>${itemName}</td><td>${price}</td><td>${orderQty}<td>${total}</td></tr>`;
+        $("#orderTbody").append(row);
+        $("#lblTotal").text(fullTotal+" LKR");
+        alert("23445");
+        clearInputItems();
+
+    }else{
+
+        let rowNo = checkDuplicates(id);
+        newQty = orderQty;
+        let oldQty = parseInt($($('#orderTbody>tr').eq(rowNo).children(":eq(3)")).text());
+        let oldTotal = parseInt($($('#orderTbody>tr').eq(rowNo).children(":eq(4)")).text());
+
+        fullTotal = fullTotal-oldTotal;
+        newQty = parseInt(oldQty) + parseInt(newQty) ;
+        newTotal = newQty * price;
+        fullTotal = fullTotal + newTotal;
+
+        // ________________Update row____________________
+        $('#orderTbody tr').eq(rowNo).children(":eq(3)").text(newQty);
+        $('#orderTbody tr').eq(rowNo).children(":eq(4)").text(newTotal);
+
+        $("#lblFullTotal").text(fullTotal+" LKR");
+        alert("test");
+        clearInputItems();
+    }
+
+
+}
+
+
+/* ______________Check Duplicate Item _____________________*/
+function checkDuplicates(itemId) {
+    for (let i = 0; i < $("#orderTbody> tr").length; i++) {
+        if (itemId == $('#orderTbody').children().eq(i).children().eq(0).text()) {
+            alert(i);
+            return i;
+        }
+
+    }
+    return -1;
+}
+
+/* ________Clear Input Field on Selected Item Area ______________*/
+function clearInputItems() {
+    $("#itemIdCmb").val("");
+    $("#txtOrderItemName").val("");
+    $("#txtQtyOnHand").val("");
+    $("#txtPrice").val("");
+    $("#txtOrderQty").val("");
 }
